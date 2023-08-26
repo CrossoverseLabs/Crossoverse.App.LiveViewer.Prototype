b44b36beca5a4eade334e94ca7170af20adb052c
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-08-26 18:40:27 +0900

    feat: Update SceneTransitionContext

--- Crossoverse.App.LiveViewer/Assets/Crossoverse.App.LiveViewer/Context/SceneTransitionContext.cs
+++ Crossoverse.App.LiveViewer/Assets/Crossoverse.App.LiveViewer/Context/SceneTransitionContext.cs
@@ -1,13 +1,56 @@
 using System;
 using Crossoverse.App.LiveViewer.Configuration;
 using Crossoverse.Toolkit.SceneTransition;
+using Cysharp.Threading.Tasks;
+using MessagePipe;
 
 namespace Crossoverse.App.LiveViewer.Context
 {
-    public sealed class SceneTransitionContext : SceneTransitionContext<StageName, SceneName>
+    public interface ISceneTransitionContext
     {
-        public SceneTransitionContext(SceneConfiguration sceneConfiguration) : base(sceneConfiguration)
+        ISubscriber<float> OnLoadingProgressUpdated { get; }
+        UniTask LoadStageAsync(StageName nextStageId);
+        UniTask LoadGlobalScenesAndInitialStageAsync();
+    }
+
+    public sealed class SceneTransitionContext : SceneTransitionContext<StageName, SceneName>, ISceneTransitionContext
+    {
+        public ISubscriber<float> OnLoadingProgressUpdated { get; }
+
+        private readonly IDisposablePublisher<float> _loadingProgressPublisher;
+
+        public SceneTransitionContext
+        (
+            SceneConfiguration sceneConfiguration,
+            EventFactory eventFactory
+        ) : base(sceneConfiguration)
         {
+            (_loadingProgressPublisher, OnLoadingProgressUpdated) = eventFactory.CreateEvent<float>();
+        }
+
+        public async UniTask LoadStageAsync(StageName nextStageId)
+        {
+            foreach (var stage in _stages)
+            {
+                if (stage.StageId == nextStageId)
+                {
+                    await LoadStageAsync(stage, Progress.Create<float>(value => 
+                    {
+                        UnityEngine.Debug.Log($"<color=lime>[{nameof(SceneTransitionContext)}] Stage loading progress: {value}</color>");
+                        _loadingProgressPublisher.Publish(value);
+                    }));
+                    return;
+                }
+            }
+        }
+
+        public async UniTask LoadGlobalScenesAndInitialStageAsync()
+        {
+            await base.LoadGlobalScenesAndInitialStageAsync(Progress.Create<float>(value => 
+            {
+                UnityEngine.Debug.Log($"<color=lime>[{nameof(SceneTransitionContext)}] Stage loading progress: {value}</color>");
+                _loadingProgressPublisher.Publish(value);
+            }));
         }
     }
 }
